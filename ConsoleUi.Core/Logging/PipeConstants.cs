5c70f5d67552794846a8982d4f9c3bb86e5387bb
DavidJanssonDev <138443239+DavidJanssonDev@users.noreply.github.com>
2026-01-22 13:52:43 +0100

    Adding Documentation

--- ConsoleUi.Core/Logging/PipeConstants.cs
+++ ConsoleUi.Core/Logging/PipeConstants.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ConsoleUi.Core.Logging;
 
+/// <summary>
+/// The name of the "tube" both apps agree to use.
+/// If you change this string, BOTH the apps must the same new value.
+/// </summary>
 public static class PipeConstants
 {
     public const string PipeName = "ConsoleUiLoggerPipe";
